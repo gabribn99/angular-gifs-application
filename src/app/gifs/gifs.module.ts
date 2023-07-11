4f6790bc91839386bcd313557bbc117c052f861c
Gabriel Ballester Niebla <56097204+gabribn99@users.noreply.github.com>
2023-07-11 11:52:16 +0100

    Fixed lazy load

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { ListComponent } from './components/card-list/card-list.component';
+import { CardComponent } from './components/card/card.component';
 
 
 
@@ -10,10 +14,12 @@ import { ListComponent } from './components/card-list/card-list.component';
   declarations: [
     HomePageComponent,
     SearchBoxComponent,
-    ListComponent
+    ListComponent,
+    CardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],exports:[
     HomePageComponent
   ]
